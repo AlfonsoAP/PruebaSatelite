6c43f20afd0ec19da032da005b85890e6ff91cab
Alfonso's Laptop <luisalfonso.alvarez@upaep.edu.mx>
2025-03-20 10:43:21 -0600

    Se agregan las cosas de SocketIO

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask;
+from flask import Flask, render_template;
+from flask_socketio import SocketIO;
 
 app = Flask(__name__);
+app.config['SECRET_KEY'] = 'jijijija';
+
+socketio = SocketIO(app);
 
 @app.route('/')
 def hello_world():
     return 'Adios Mundo';
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client Connected');
+
+@socketio.on('message')
+def handle_message(data):
+    print('Received message:', data);
+    socketio.emit('response', 'The server received your message: ' + data);
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True);
